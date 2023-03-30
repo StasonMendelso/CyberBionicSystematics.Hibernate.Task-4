fe6f70a1e7909080da485f40111cdf568432d021
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-30 18:31:59 +0300

    initial commit: the definition of the task

---
+++ task 6/src/main/java/org/stanislav/DemoTask6.java
@@ -0,0 +1,13 @@
+package org.stanislav;
+
+/**
+ * Hello world!
+ *
+ */
+public class DemoTask6
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ task 7/src/main/java/org/stanislav/DemoTask7.java
@@ -0,0 +1,13 @@
+package org.stanislav;
+
+/**
+ * Hello world!
+ *
+ */
+public class DemoTask7
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
